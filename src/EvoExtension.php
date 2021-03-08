4f17a5f16a50253573252a3a09768e023db18578
Pathologic <m@xim.name>
2021-03-08 16:47:44 +0300

    bump

--- src/EvoExtension.php
+++ src/EvoExtension.php
@@ -4,19 +4,38 @@
 namespace Pathologic\EvoTwig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
-class EvoExtension extends AbstractExtension
+class EvoExtension extends AbstractExtension implements GlobalsInterface
 {
     protected $modx;
+    protected $params = [];
 
-    public function __construct(\DocumentParser $modx)
+    public function __construct(\DocumentParser $modx, array $params = [])
     {
         $this->modx = $modx;
+        $this->params = $params;
     }
 
-    public function getFunctions()
+    public function getGlobals(): array
+    {
+        return [
+            'modx'           => $this->modx,
+            'documentObject' => $this->modx->documentObject,
+            'resource'       => $this->getResource(),
+            'debug'          => $this->params['debug'],
+            'config'         => $this->modx->config,
+            'ajax'           => isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest',
+            '_GET'           => $_GET,
+            '_POST'          => $_POST,
+            '_REQUEST'       => $_POST,
+            '_COOKIE'        => $_COOKIE,
+        ];
+    }
+
+    public function getFunctions(): array
     {
         $functions = [];
         $functions[] = new TwigFunction('makeUrl', [
@@ -39,7 +58,7 @@ class EvoExtension extends AbstractExtension
         return $functions;
     }
 
-    public function getFilters()
+    public function getFilters(): array
     {
         $filters = [];
         $filters[] = new TwigFilter('modxParser', [
@@ -50,12 +69,12 @@ class EvoExtension extends AbstractExtension
         return $filters;
     }
 
-    public function makeUrl($id, array $args = [], $absolute = true)
+    public function makeUrl($id, array $args = [], $absolute = false): string
     {
         return $this->modx->makeUrl($id, '', http_build_query($args), $absolute ? 'full' : '');
     }
 
-    public function modxParser($content)
+    public function modxParser($content): string
     {
         $this->modx->minParserPasses = 2;
         $this->modx->maxParserPasses = 10;
@@ -66,5 +85,15 @@ class EvoExtension extends AbstractExtension
         $this->modx->maxParserPasses = -1;
 
         return $out;
+    }
+
+    private function getResource()
+    {
+        $resource = [];
+        foreach ($this->modx->documentObject as $key => $value) {
+            $resource[$key] = is_array($value) ? $value[1] : $value;
+        }
+
+        return $resource;
     }
 }
